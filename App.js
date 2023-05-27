db65db8c8dbbc70deb818daa735ed24386285475
Serhii Lytvynenko <mr.litvinenko@gmail.com>
2023-05-27 18:40:29 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,17 +1,16 @@
 import { StyleSheet, Text, View } from "react-native";
-import { useFonts } from 'expo-font';
+import { useFonts } from "expo-font";
 // import StatusBarFn from './Components/StatusBar'
 import RegistrationScreen from "./Components/Screens/RegistrationScreen";
 
 export default function App() {
+    const [fontsLoaded] = useFonts({
+        Roboto: require("./assets/fonts/Roboto/Roboto-Black.ttf"),
+    });
 
-  const [fontsLoaded] = useFonts({
-    'Roboto': require('./assets/fonts/Roboto/Roboto-Black.ttf'),
-  });
-
-  if (!fontsLoaded) {
-    return null;
-  }
+    if (!fontsLoaded) {
+        return null;
+    }
 
     return (
         <View style={styles.container}>
